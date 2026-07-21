6a8788c6725efc99dc1730918b71afe27bccc630
Laura <lbmergenova@gmail.com>
2026-07-21 13:49:56 +0500

    Реализована trim — удаление пробелов с начала и конца строки

---
+++ string-utils/trim.js
@@ -0,0 +1,30 @@
+import { len } from "./len.js";
+import { slice } from "./slice.js";
+
+/**
+ * Удаляет пробелы с начала и конца строки.
+ *
+ * @param {string} str - Исходная строка .
+ * @returns {string} - Строка без пробелов в начале и конце.
+ * @throws {TypeError} - Если аргумент не является строкой.
+ *
+ * @example
+ *   trim('  hello  ');   // 'hello'
+ *   trim('hello');       // 'hello'
+ *   trim('   ');         // ''
+ */
+export function trim(str) {
+    let start = 0;
+    let end = len(str) - 1;
+    while (str[start] === ' ') {
+        start++;
+    }
+    if (end === start) 
+        return '';
+    while (str[end] === ' ') {
+        end--;
+    }
+    return slice(str, start, ++end);
+}
+
+
